23d1a2daf9d9892f02f72afc1df6fa0838b6e72e
williamqarmstrong <williamqarmstrong@gmail.com>
2018-04-10 21:21:35 -0400

    Initital Commit
    Initital Commit

---
+++ FMC/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FMC
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
